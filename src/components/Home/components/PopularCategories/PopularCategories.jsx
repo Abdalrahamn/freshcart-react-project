f4bf5254e91ebf49b6ee6be4c5de4fa973d2de2c
abdelrahman.mahfouz@areebgroup.com <abdelrahman.mahfouz@areebgroup.com>
2025-09-10 02:06:23 +0300

    adding slider for product details and popular category

---
+++ src/components/Home/components/PopularCategories/PopularCategories.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import styles from "./PopularCategories.module.css";
+
+export default function PopularCategories() {
+  const [categories, setCategories] = useState([]);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://ecommerce.routemisr.com/api/v1/categories")
+      .then((res) => {
+        setCategories(res.data.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="my-10 px-10">
+        <h2 className="text-center text-2xl font-bold mb-10">
+          Popular Categories
+        </h2>
+        <Slider {...settings}>
+          {categories.map((category) => (
+            <div key={category._id}>
+              <img
+                src={category.image}
+                alt={category.name}
+                className={styles.categoryImage}
+              />
+              <h3 className="text-center">{category.name}</h3>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </>
+  );
+}
